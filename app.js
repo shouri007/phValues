5600e99ce074897cf089d0dfce502438f636ac3f
Brijesh Daiya <bbdaiya@gmail.com>
2017-05-10 22:58:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 var express = require('express');
 var config = require('./server/configure');
 var path = require('path');
-var mongoose = require('mongoose');
+var pg = require('pg');
 
 var port = 3000 || process.env.PORT;
 var app = express();
 app = config(app);
-mongoose.connect('mongodb://localhost/phvalues');
-mongoose.connection.on('open',function (){
-    console.log('Mongooose connected');
-});
+
 
 app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname, 'public')));
