10f3ebfe7c35def47af4c6ac232fd178433c6183
Brijesh Daiya <bbdaiya@gmail.com>
2017-05-10 22:55:52 +0530

    Migrate to SQL

--- models/phvalue.js
+++ models/phvalue.js
@@ -1,11 +1,10 @@
-var mongoose = require('mongoose');
-var schema = mongoose.Schema;
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/values';
 
-//schema for a pH value. Value and date added are the attributes.
-var phvalueSchema = new schema({
-	value:{type:Number},
-    dateadded: {type:Date}
+var client = new pg.Client(connectionString);
+client.connect();
+var query = client.query(
+  'CREATE TABLE values(id SERIAL PRIMARY KEY, value NUMERIC(5,2) not null, data_added TIMESTAMP)');
+query.on('end', function(){
+	client.end();
 });
-
-//module exports
-module.exports = mongoose.model('phvalue', phvalueSchema);
